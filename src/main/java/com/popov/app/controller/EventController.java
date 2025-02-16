95d47ac7d65f69348d558983dff724bd3075cd6f
Yarik22 <popov.yarik.popov@gmail.com>
2025-02-16 15:39:06 +0200

    lr5

--- src/main/java/com/popov/app/controller/EventController.java
+++ src/main/java/com/popov/app/controller/EventController.java
@@ -12,7 +12,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-import java.io.Console;
 import java.util.List;
 import java.util.UUID;
 import java.util.stream.Collectors;
--- src/main/java/com/popov/app/model/Event.java
+++ src/main/java/com/popov/app/model/Event.java
@@ -1,7 +1,6 @@
 package com.popov.app.model;
 
 
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.UUID;
 
